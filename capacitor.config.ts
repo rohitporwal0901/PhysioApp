efadc8b79554b63a2657c5ef43b56ca23a0ce48a
rohitQpl <rohit@quadralyst.com>
2026-03-16 17:04:55 +0530

    fix apk build porcess

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.physioapp.app',
+  appName: 'PhysioApp',
+  webDir: 'dist/physio-app/browser'
+};
+
+export default config;
