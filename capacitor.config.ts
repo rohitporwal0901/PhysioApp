4e581ceb67587b78ee9f7cc8adf3333b1c0680df
rohitQpl <rohit@quadralyst.com>
2026-04-03 18:36:11 +0530

    chanegs apk name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.physioapp.app',
-  appName: 'PhysioApp',
+  appName: 'Health Hub',
   webDir: 'dist/physio-app/browser'
 };
 
